a0a367b29de0e0a3500af95511c2e89672356ab9
Jeneva Besin <jenevabesin16@gmail.com>
2024-09-16 11:16:54 +0800

    0914_6_pass-data-to-views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,5 @@
    <x-slot:heading>
     Home Page
    </x-slot:heading>
-   <h1> This page is from Home Page!!</h1>
+  <h1>This page is from Home Page!!</h1>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,60 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Arr; // Import the Arr class
+
 
 Route::get('/', function () {
-    return view('home');
+  return View('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            [
+                'id'=> 1,
+              'title' => 'Teacher',
+              'salary' => '$10000'
+            ],
+            [
+                'id'=> 2,
+              'title' => 'Programmer',
+              'salary' => '$60000'
+            ],
+            [
+                'id'=> 3,
+              'title' => 'QA',
+              'salary' => '$40000'
+            ]
+        ]
+      ]);
+  });
+
+
+  Route::get('/jobs/{id}', function ($id) {
+$jobs = [
+        [
+            'id'=> 1,
+          'title' => 'Teacher',
+          'salary' => '$10000'
+        ],
+        [
+            'id'=> 2,
+          'title' => 'Programmer',
+          'salary' => '$60000'
+        ],
+        [
+            'id'=> 3,
+          'title' => 'QA',
+          'salary' => '$40000'
+        ]
+        ];
+
+        $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+
+    return view('job', ['job' => $job]);
 });
+
 
 Route::get('/contact', function () {
     return view('contact');
