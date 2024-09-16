9f062125000b59e16cf767918e7f638cff23805d
Jeneva Besin <jenevabesin16@gmail.com>
2024-09-16 12:48:46 +0800

    0914_7_autoloading-namespaces-and-models

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Arr; // Import the Arr class
-
+use App\Models\Job;
 
 Route::get('/', function () {
   return View('home');
@@ -10,51 +9,15 @@ Route::get('/', function () {
 
 Route::get('/jobs', function () {
     return view('jobs', [
-        'jobs' => [
-            [
-                'id'=> 1,
-              'title' => 'Teacher',
-              'salary' => '$10000'
-            ],
-            [
-                'id'=> 2,
-              'title' => 'Programmer',
-              'salary' => '$60000'
-            ],
-            [
-                'id'=> 3,
-              'title' => 'QA',
-              'salary' => '$40000'
-            ]
-        ]
+        'jobs' => Job::all()
       ]);
   });
 
-
-  Route::get('/jobs/{id}', function ($id) {
-$jobs = [
-        [
-            'id'=> 1,
-          'title' => 'Teacher',
-          'salary' => '$10000'
-        ],
-        [
-            'id'=> 2,
-          'title' => 'Programmer',
-          'salary' => '$60000'
-        ],
-        [
-            'id'=> 3,
-          'title' => 'QA',
-          'salary' => '$40000'
-        ]
-        ];
-
-        $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+Route::get('/jobs/{id}', function ($id){
+       $job = Job::find($id);
 
     return view('job', ['job' => $job]);
 });
-
 
 Route::get('/contact', function () {
     return view('contact');
