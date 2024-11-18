33a7a11ae86305fec47d24c52e098ac58adf805d
Jenava Besin <jenevabesin16@gmail.com>
2024-11-18 15:48:40 +0800

    1116_28_techniques_for_building_tailwind_projects

--- resources/views/components/section-heading.blade.php
+++ resources/views/components/section-heading.blade.php
@@ -1,1 +1,5 @@
-<a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">{{ $slot }}</a>
+<div class="inline-flex items-center gap-x-2">
+    <span class="w-2 h-2 bg-white inline-block"></span>
+
+    <h3 class="font-bold text-lg">{{ $slot }}</h3>
+</div>
--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,4 +1,16 @@
-<div class="inline-flex items-center gap-x-2">
-    <span class="w-2 h-2 bg-white inline-block"></span>
-    <h3 class="font-bold text-lg">{{ $slot }}</h3>
-</div>
+
+@props(['size' => 'base'])
+
+@php
+    $classes = "bg-white/10 hover:bg-white/25 rounded-xl font-bold transition-colors duration-300";
+
+    if ($size === 'base') {
+        $classes .= " px-5 py-1 text-sm";
+    }
+
+    if ($size === 'small') {
+        $classes .= " px-3 py-1 text-2xs";
+    }
+@endphp
+
+<a href="#" class="{{ $classes }}">{{ $slot }}</a>
